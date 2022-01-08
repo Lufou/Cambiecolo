dcb60b3963fdb5a3f1525dd35ca24523ce93b612
Lufou <lucas.damase@laposte.net>
2022-01-08 11:21:38 -0500

    Good work so far

--- game.py
+++ game.py
@@ -1,26 +1,83 @@
 import sysv_ipc
-import threading
+from stoppable_thread import StoppableThread
 from multiprocessing import shared_memory
+import sys
+import random
+import signal
+import os
 import time
 
-print("Launching game process...")
+typeTransport = ['pied','velo','voiture','train','avion']
 shm_a = shared_memory.SharedMemory(create=True, size=5)
+playersNumber = 0
+cardCounter = {}
+messageQueues = []
+mqThread = ""
 
+def chooseRandomCards():
+    cartes = ""
+    for j in range (0,5):
+        k = random.randint(0,playersNumber-1)
+        if typeTransport[k] not in cardCounter:
+            cardCounter[typeTransport[k]] = 0
+        while cardCounter[typeTransport[k]] == 5:
+            k = random.randint(0,playersNumber-1)
+        cartes += typeTransport[k] + ","
+        cardCounter[typeTransport[k]] += 1
+    cartes.removesuffix(",")
+    return cartes
+        
 def readMq(mq):
     while True:
         print("Waiting for msg")
-        message, t = mq.receive(True, 1)
+        message, t = mq.receive(True, 2)
         value = message.decode()
         print("Received "+value)
         value = value.split(" ")
         if value[0] == "hello":
             print("Received hello from "+value[1])
-            return_message = f"{shm_a.name} velo,velo,voiture".encode()
-            mq.send(return_message, True, 0)
-for i in range(1,6):
-    key = 128+i
-    messageQueue = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
-    print(f"Message Queue {key} created")
-    mqThread = threading.Thread(target=readMq, args = (messageQueue,))
-    mqThread.start()
-    print("Thread created")
+            return_message = f"{shm_a.name} {chooseRandomCards()}".encode()
+            mq.send(return_message, True, 1)
+def terminate():
+    print("Stopping mqReading thread")
+    mqThread.terminate() # Terminate the thread readMq which read the messageQueue as we're going to destroy the mq
+    print("Broadcasting termination to all clients")
+    for mq in messageQueues:
+        mq.send("terminate".encode(), True, 1) # Broadcast to all connected clients we are going to close the connection
+    time.sleep(1)
+    for mq in messageQueues:
+        mq.remove()
+    print("Removed message queues")
+    shm_a.close()
+    print("SharedMemory closed")
+    shm_a.unlink()
+    print("Destroyed SharedMemory")
+    print("Closing...")
+    os._exit(0)
+def keyboardInterruptHandler(signal, frame):
+   terminate()
+def initGame():
+    global playersNumber
+    global mqThread
+    if len(sys.argv) != 2:
+        print("Incorrect amount of arguments")
+        sys.exit(2)
+    number = sys.argv[1]
+    try:
+        number = int(number)
+    except ValueError:
+        print("Incorrect number of players.")
+        sys.exit(2)
+    playersNumber = number
+    for i in range(1,playersNumber+1):
+        key = 128+i
+        messageQueue = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+        messageQueues.append(messageQueue)
+        print(f"Message Queue {key} created")
+        mqThread = StoppableThread(target=readMq, args = (messageQueue,))
+        mqThread.start()
+        print("Thread created")
+    signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+print("Launching game process...")
+initGame()
--- player.py
+++ player.py
@@ -4,40 +4,61 @@ import os
 from multiprocessing import shared_memory
 import threading
 
-print("Starting player process")
+myCards = []
 pid = -1
-shm_key = ""
-try:
-    pid = int(sys.argv[1])
-except ValueError:
-    print("Incorrect pid passed.")
-    os.Exit(2)
+messageQueue = ""
+sharedMemory = ""
 
 def readMq(mq):
-    global shm_key
     while True:
-        message, t = mq.receive()
-        value = message.decode().split(" ")
+        try:
+            message, t = mq.receive(True, 1)
+            value = message.decode()
+            if value == "terminate":
+                print("Server decided to close the connection.")
+                sharedMemory.close()
+                os._exit(0)
+            print("Game is talking to me")
+        except sysv_ipc.ExistentialError:
+            print("MessageQueue has been destroyed, connection has been closed.")
+            sharedMemory.close()
+            os._exit(1)
 
-        print("Game is talking to me")
+def initPlayer():
+    global pid
+    global messageQueue
+    global sharedMemory
+    try:
+        pid = int(sys.argv[1])
+    except ValueError:
+        print("Incorrect pid passed.")
+        os.Exit(2)
+   
+    mq_key = 128+pid
+    try:
+        messageQueue = sysv_ipc.MessageQueue(mq_key)
+    except sysv_ipc.ExistentialError:
+        print("Game process do not accept your Player ID.")
+        os._exit(1)
+    print("Connected to MessageQueue")
+    msg = "hello "+str(pid)
+    messageQueue.send(str(msg).encode(), True, 2)
+    print("Message '"+msg+"' sended")
 
-mq_key = 128+pid
-mq = sysv_ipc.MessageQueue(mq_key)
-print("Connected to MessageQueue")
-msg = "hello "+str(pid)
-mq.send(str(msg).encode())
-print("Message '"+msg+"' sended")
+    print("Waiting for my cards")
+    message, t = messageQueue.receive(True, 1)
+    value = message.decode()
+    print("Received "+value)
+    value = value.split(" ")
+    cards_string = value[1].split(",")
+    for card in cards_string:
+        myCards.append(card)
+    shm_key = value[0]
+    sharedMemory = shared_memory.SharedMemory(shm_key)
+    print("Connected to shared mem")
+    mqThread = threading.Thread(target=readMq, args = (messageQueue,))
+    mqThread.start()
+    print("Thread started")
 
-print("Waiting for my cards")
-message, t = mq.receive()
-value = message.decode()
-print("Received "+value)
-value = value.split(" ")
-shm_key = value[0]
-shm_b = shared_memory.SharedMemory(shm_key)
-print("Connected to shared mem")
-
-
-mqThread = threading.Thread(target=readMq, args = (mq,))
-mqThread.start()
-print("Thread started")
+print("Starting player process")
+initPlayer()
